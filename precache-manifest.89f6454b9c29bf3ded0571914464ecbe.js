1f9e0d1e3c1d7a23f1a8c54808c9e6602769fd92
hana Charaabi <charaabihana90@gmail.com>
2019-05-09 14:26:08 +0100

    Updates

---
+++ precache-manifest.89f6454b9c29bf3ded0571914464ecbe.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e5e5c59fd3f15d047cd18db628e302",
+    "url": "/docplannerReact/index.html"
+  },
+  {
+    "revision": "c5bab42b6c5d070a0972",
+    "url": "/docplannerReact/static/css/main.122e178f.chunk.css"
+  },
+  {
+    "revision": "0974015e94b5eff7d38d",
+    "url": "/docplannerReact/static/js/2.809ee080.chunk.js"
+  },
+  {
+    "revision": "c5bab42b6c5d070a0972",
+    "url": "/docplannerReact/static/js/main.34ff9225.chunk.js"
+  },
+  {
+    "revision": "6c17e8b18fde78ceacf4",
+    "url": "/docplannerReact/static/js/runtime~main.ca513ad7.js"
+  },
+  {
+    "revision": "30fef32f18881c2811fe20f9d731bab2",
+    "url": "/docplannerReact/static/media/Barcelona.30fef32f.png"
+  },
+  {
+    "revision": "2ab726ec8c70ef89de611916f1bd5a57",
+    "url": "/docplannerReact/static/media/Curitiba.2ab726ec.png"
+  },
+  {
+    "revision": "a772a418e427f7945447d5e0f8f185cf",
+    "url": "/docplannerReact/static/media/Istanbul.a772a418.png"
+  },
+  {
+    "revision": "c92e8f5856e4439678226216d400b01c",
+    "url": "/docplannerReact/static/media/Mexico City.c92e8f58.png"
+  },
+  {
+    "revision": "16430b36ccdd96c0b1fd2757733f29bb",
+    "url": "/docplannerReact/static/media/Rome.16430b36.png"
+  },
+  {
+    "revision": "4a92ae162eaf6d061ee4f16ced26fc66",
+    "url": "/docplannerReact/static/media/Warsaw.4a92ae16.png"
+  },
+  {
+    "revision": "edb44798eed9e9d14295955d5e44963a",
+    "url": "/docplannerReact/static/media/background.edb44798.jpg"
+  },
+  {
+    "revision": "8e5b345ff9df62455087a329d3b48498",
+    "url": "/docplannerReact/static/media/logo-default-group-en.8e5b345f.svg"
+  },
+  {
+    "revision": "82d323de468a8ad9636c43e8de2a26ba",
+    "url": "/docplannerReact/static/media/screen-marketplace.82d323de.png"
+  },
+  {
+    "revision": "120076c7e404fb282e53f6885f38dac7",
+    "url": "/docplannerReact/static/media/screen-saas.120076c7.png"
+  }
+]);
